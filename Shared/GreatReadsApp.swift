fa4249dcec15040b6c155f320e5e581f1da1e11e
Luissh22 <luisabraham_11@hotmail.com>
2020-08-08 18:09:35 -0400

    Initial Commit

---
+++ Shared/GreatReadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreatReadsApp.swift
+//  Shared
+//
+//  Created by Luis Abraham on 2020-08-08.
+//
+
+import SwiftUI
+
+@main
+struct GreatReadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
